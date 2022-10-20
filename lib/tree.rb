8de15314f7c323e238d7cc05f29aa843a022bca3
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-20 22:33:28 +0300

    Create a tree class

---
+++ lib/tree.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative 'node'
+
+class Tree
+  def initialize(array)
+    @root = build_tree(array)
+  end
+
+  def build_tree(array)
+    root =
+  end
+end
