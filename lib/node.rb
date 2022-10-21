fb07b765a5ecf845440c7d732a69e5b35daf3c9e
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-21 17:34:25 +0300

    Add attribute accessprs

--- lib/node.rb
+++ lib/node.rb
@@ -2,6 +2,8 @@
 
 # Object which holds a single element of data and a pointer to the left and right node in the tree.
 class Node
+  attr_accessor :data, :right, :left
+
   def initialize(data = nil, left = nil, right = nil)
     @data = data
     @right = right
