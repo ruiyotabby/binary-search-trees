e80e784c2c53869853891839e63c9540079c6cac
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-21 17:37:17 +0300

    Create Tree class

--- lib/tree.rb
+++ lib/tree.rb
@@ -3,11 +3,15 @@
 require_relative 'node'
 
 class Tree
+  attr_reader :root
+
   def initialize(array)
     @root = build_tree(array)
   end
-
-  def build_tree(array)
-    root =
-  end
 end
+
+array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+t = Tree.new array
+p array
+# p t.root.right.right.left
+print t.pretty_print
